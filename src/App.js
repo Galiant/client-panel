0f6c14683b80f995abad01140c807b8bb67aa6b5
Galiant <antonijodev@gmail.com>
2019-07-31 23:04:33 +0100

    Folder structure setup and added  AppNavbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavbar from './components/layout/AppNavbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <AppNavbar />
+        <div className="container">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </Router>
   );
 }
 
